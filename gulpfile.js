095999d876a7288bfa876d974ec53b2aa6d1e817
zadorotskas <zadorotskas@yandex.ru>
2021-12-14 15:08:24 +0300

    homework 5 fixes

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ gulp.task('rollup', async (done) => {
     const bundle = await rollup.rollup(rollupConfig);
 
     bundle.write({
-        format: 'esm',
+        format: 'iife',
         file: 'dist/app.js'
     });
 
